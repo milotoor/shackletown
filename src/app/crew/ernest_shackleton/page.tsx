84338f19685a7b93794e9188fe51c934becbb516
Milo Toor <milo@synop.ai>
2025-02-23 21:32:26 -0800

    Favicon

--- src/app/crew/ernest_shackleton/page.tsx
+++ src/app/crew/ernest_shackleton/page.tsx
@@ -6,8 +6,16 @@ import useCrewBio from "@/hooks/useCrewBio";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Ernest Shackleton - Expedition Leader",
-  description: "Commander of the most successful failure of all time.",
+  openGraph: {
+    title: "Ernest Shackleton - Expedition Leader",
+    description: "Commander of the most successful failure of all time.",
+    images: [
+      {
+        url: "/crew/ernest_shackleton/img/01.jpg",
+        alt: "Ernest Shackleton",
+      },
+    ],
+  },
 };
 
 const quotes: QuoteProps[] = [
